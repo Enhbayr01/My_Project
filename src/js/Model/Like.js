94dd040d61b3f23ba6ef3db31d00071bee6d518d
Enhbayr <enhbayr745@gmail.com>
2025-01-13 21:05:11 +0800

    ok

--- src/js/Model/Like.js
+++ src/js/Model/Like.js
@@ -1,40 +1,39 @@
-export default class Likes{
-    constructor(){
-        this.readDAtaFromLocalStorage();
+export default class Likes {
+  constructor() {
+    this.readDAtaFromLocalStorage();
 
-        if(!this.likes) this.likes = [];
-    }
+    if (!this.likes) this.likes = [];
+  }
 
-    addLike(id, title, publisher, img){
-        const like = {id, title, publisher, img};
-        this.likes.push(like);
-        this.saveDataToLocalStorage();
-        return like;
-    }
+  addLike(id, title, publisher, img) {
+    const like = { id, title, publisher, img };
+    this.likes.push(like);
+    this.saveDataToLocalStorage();
+    return like;
+  }
 
-   
-    deleteLike(id){
-        const index = this.likes.findIndex(el => el.id ===id);
+  deleteLike(id) {
+    const index = this.likes.findIndex((el) => el.id === id);
 
-        this.likes.splice(index, 1);
-        this.saveDataToLocalStorage();
-        }  
-     isLiked(id){
-        // if (this.likes.findIndex(el => el.id === id) === -1) return false;
-        // else return true;
+    this.likes.splice(index, 1);
+    this.saveDataToLocalStorage();
+  }
+  isLiked(id) {
+    // if (this.likes.findIndex(el => el.id === id) === -1) return false;
+    // else return true;
 
-        return this.likes.findIndex(el => el.id === id) !== -1;
-     }   
+    return this.likes.findIndex((el) => el.id === id) !== -1;
+  }
 
-     getNumberOfLikes(){
-        return this.likes.length;
-     }
+  getNumberOfLikes() {
+    return this.likes.length;
+  }
 
-     saveDataToLocalStorage(){
-        localStorage.setItem("likes", JSON.stringify(this.likes));
-     }
+  saveDataToLocalStorage() {
+    localStorage.setItem("likes", JSON.stringify(this.likes));
+  }
 
-     readDAtaFromLocalStorage(){
-      this.likes = JSON.parse(localStorage.getItem('likes'));
-     }
+  readDAtaFromLocalStorage() {
+    this.likes = JSON.parse(localStorage.getItem("likes"));
+  }
 }
